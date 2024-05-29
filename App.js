86737058e573422169b9e35b1a453355539cd9d8
Deswin77 <105841109321@student.unismuh.ac.id>
2024-05-29 14:43:40 +0800

    Selesai

--- App.js
+++ App.js
@@ -3,16 +3,47 @@ import React from "react";
 
 const App = () => {
   return (
-    <View style={{flex: 1}}>
-      <View style={{flex: 1, flexDirection: "row", backgroundColor: "white"}}>
-      <View style={{flex: 1, backgroundColor: "yellow"}}></View>
-      <View style={{flex: 1, backgroundColor: "green"}}></View>
+    <View style={{ flex: 1}}>
+      <View style={{ flexDirection: "row", 
+      flex: 1,
+      alignItems: "flex-end", //baris
+      justifyContent: "center", //kolom
+      marginLeft: 8, //jarak dari kiri
+      marginRight: 8, //jarak dari kanan
+      marginBottom: 40, //jarak dari bawah
+    }}>
+      <View style={{
+        backgroundColor: "red", width: 180, height: 80,
+        justifyContent: "center",
+        borderRadius: 10,
+        
+      }}>
+        <Text style={{
+          fontSize: 30, color: "white",
+          textAlign: "center",
+          fontWeight: "bold",
+        }}>
+          Login
+        </Text>
       </View>
-      <View style={{flex: 1, backgroundColor: "red"}}></View>
-      <View style={{flex: 1, backgroundColor: "blue"}}></View>
+      <View style={{
+        backgroundColor: "blue", width: 180, height: 80,
+        justifyContent: "center",
+        borderRadius: 10,
+        marginLeft: 8,
+      }}>
+         <Text style={{
+          fontSize: 30, color: "white",
+          textAlign: "center",
+          fontWeight: "bold",
+        }}>
+          Register
+        </Text>
+      </View>
     </View>
+  </View>
       )
-      }
+    }
 
 
 export default App;
