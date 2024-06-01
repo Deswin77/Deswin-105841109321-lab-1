9984c26cf83644f4cfd6381f9632c04421cfe4da
Deswin77 <105841109321@student.unismuh.ac.id>
2024-06-02 01:53:32 +0800

    tugas3

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
 const App = () => {
   return (
+    <View style={{ flex: 1 }}>
+      <View style={{
+        backgroundColor: "white",
+        top:0,
+        bottom:0,
+        height:60,
+        justifyContent:'center'
+        }}>
+        <Text style={{
+          fontSize: 30,
+          textAlign: "center",
+          fontWeight: "bold",
+          color: "black",
+        }}>
+          WELCOME
+        </Text>
+        </View>
+        <View style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          top: '5%',
+        }}>
+          <Image source={require("./assets/shopee.png")} style={{
+            width: '80%',
+            height: '100%',
+            resizeMode: 'contain',
+          }} />
+        </View>
     <View style={{ flex: 1}}>
       <View style={{ flexDirection: "row", 
       flex: 1,
-      alignItems: "flex-end", //baris
+      alignItems: "flex-start", //baris
       justifyContent: "center", //kolom
       marginLeft: 8, //jarak dari kiri
       marginRight: 8, //jarak dari kanan
@@ -23,7 +52,7 @@ const App = () => {
           textAlign: "center",
           fontWeight: "bold",
         }}>
-          Login
+          Email/Telepon
         </Text>
       </View>
       <View style={{
@@ -37,10 +66,21 @@ const App = () => {
           textAlign: "center",
           fontWeight: "bold",
         }}>
-          Register
+          Password
         </Text>
       </View>
     </View>
+    <Text style={{
+          fontSize: 30,
+          textAlign: "center",
+          fontWeight: "bold",
+          color: "black",
+          bottom: '60%',
+          backgroundColor: "orange",
+        }}>
+          Login
+        </Text>
+  </View>
   </View>
       )
     }
